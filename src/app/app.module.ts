a3864ecb58054db2406984694acb3869bef18f58
Drew Norman <drew.norman@camunda.com>
2020-05-14 17:34:37 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CoreModule } from './core/core.module';
+import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from  './shared/shared.module';
+import { AppComponent }  from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
